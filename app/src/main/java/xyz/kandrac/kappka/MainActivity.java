1c53a8390519d9c923cc587fb47316d5935d0b7f
Jan Kandrac <jan@kandrac.xyz>
2017-02-09 15:49:28 +0100

    Main Fragment defined

--- app/src/main/java/xyz/kandrac/kappka/MainActivity.java
+++ app/src/main/java/xyz/kandrac/kappka/MainActivity.java
@@ -3,11 +3,18 @@ package xyz.kandrac.kappka;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +31,47 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // set viewpager
+        mViewPager = (ViewPager) findViewById(R.id.content_pager);
+        mViewPager.setAdapter(new TabPagerAdapter(getSupportFragmentManager()));
+
+        // set tabs
+        final TabLayout tabs = (TabLayout) findViewById(R.id.main_tab_holder);
+        tabs.setupWithViewPager(mViewPager);
+    }
+
+
+    private class TabPagerAdapter extends FragmentStatePagerAdapter {
+
+        private final Fragment[] items = new Fragment[]{
+                EatFragment.getInstance(),
+                SleepFragment.getInstance(),
+                PoopFragment.getInstance()
+        };
+
+        private TabPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return items[position];
+        }
+
+        @Override
+        public int getCount() {
+            return items.length;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position){
+                case 0: return getString(R.string.main_tab_eat);
+                case 1: return getString(R.string.main_tab_sleep);
+                case 2: return getString(R.string.main_tab_poop);
+                default: return null;
+            }
+        }
     }
 }
---
+++ app/src/main/java/xyz/kandrac/kappka/SleepFragment.java
@@ -0,0 +1,24 @@
+package xyz.kandrac.kappka;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by jan on 9.2.2017.
+ */
+public class SleepFragment extends Fragment {
+
+    public static SleepFragment getInstance() {
+        return new SleepFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_sleep, container, false);
+    }
+}
