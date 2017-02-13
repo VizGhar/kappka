bdc2bf9e3d352d68adf7a3039a1269627d779ff0
Jan Kandrac <jan@kandrac.xyz>
2017-02-13 19:54:10 +0100

    Firebase Database init

---
+++ app/src/main/java/xyz/kandrac/kappka/data/firebase/References.java
@@ -0,0 +1,10 @@
+package xyz.kandrac.kappka.data.firebase;
+
+/**
+ * References in firebase database
+ * <p>
+ * Created by jan on 23.10.2016.
+ */
+public interface References {
+    String FEEDBACK_REFERENCE = "feedback";
+}
